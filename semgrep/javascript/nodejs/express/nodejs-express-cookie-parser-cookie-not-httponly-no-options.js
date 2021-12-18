e8631485cef3398590d0c33a053e3b9dff5e0ce2
Jonathan Afek <jonathan@mobb.dev>
2021-12-18 22:41:23 +0200

    add es6 based imports to the httpOnly express cookie-parser rules example vulnerable code

--- semgrep/javascript/nodejs/express/nodejs-express-cookie-parser-cookie-not-httponly-no-options.js
+++ semgrep/javascript/nodejs/express/nodejs-express-cookie-parser-cookie-not-httponly-no-options.js
@@ -1,11 +1,21 @@
 //vulnerable
 express = require('express')
+import * as mod from 'cookie-parser'
+app = express()
+app.use(mod)
+
+app.get('/', (req, res) => {
+        res.cookie('v1','zxc')
+});
+
+//vulnerable
+express = require('express')
 mod = require("cookie-parser")
 app = express()
 app.use(mod)
 
 app.get('/', (req, res) => {
-        res.cookie('qwe','zxc')
+        res.cookie('v2','zxc')
 });
 
 //not vulnerable
--- semgrep/javascript/nodejs/express/nodejs-express-cookie-parser-cookie-not-httponly-options-bad-httponly.js
+++ semgrep/javascript/nodejs/express/nodejs-express-cookie-parser-cookie-not-httponly-options-bad-httponly.js
@@ -1,11 +1,24 @@
 //vulnerable
 express = require('express')
+import * as mod from 'cookie-parser'
+app = express()
+app.use(mod)
+
+app.get('/', (req, res) => {
+        res.cookie('v1','zxc', {a:a,
+                                 f:d,
+                                 httpOnly: false,
+                                 a:b})
+});
+
+//vulnerable
+express = require('express')
 mod = require("cookie-parser")
 app = express()
 app.use(mod)
 
 app.get('/', (req, res) => {
-        res.cookie('qwe','zxc', {a:a,
+        res.cookie('v2','zxc', {a:a,
                                  f:d,
                                  httpOnly: false,
                                  a:b})
